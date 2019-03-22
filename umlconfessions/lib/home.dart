4cef1adc3712bc34fa2c638db90f641f56998945
hillary <hillary_ssemakula@student.uml.edu>
2019-03-22 16:28:46 -0400

    home nav

--- umlconfessions/lib/home.dart
+++ umlconfessions/lib/home.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
 class Home extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  HomeState createState() => new HomeState();
 }
 
-class RandomWordsState extends State<Home> {
+class HomeState extends State<Home> {
   final profilePicture = 'assets/images/logo.png'; //profile picture
   var karma = 100;
   var numOfPosts = 16;
@@ -19,28 +20,60 @@ class RandomWordsState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+      appBar: AppBar(
+          title: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 33,
+                  width: 33,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage(profilePicture),
+                          fit: BoxFit.cover)),
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Home",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    ))
+              ]),
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 2),
+      body: Text("Posts list"),
+      bottomNavigationBar: ,
+    );
+  }
+}
 
-                children: <Widget>[
-              Container(
-                height: 33,
-                width: 33,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                        image: AssetImage(profilePicture), fit: BoxFit.cover)),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 20),
-              child: Text(
-                "Home",
-                style: TextStyle(color: Colors.black, fontSize: 18),
-              ))
-            ]),
-            backgroundColor: Theme.of(context).canvasColor,
-            elevation: 2),
-        body: Text("Posts List"));
+class BottomNavyBarHelper{
+  int _index = 0;
+
+  Widget build() {
+    return BottomNavyBar(items: [
+      BottomNavyBarItem(
+        icon: Icon(Icons.apps),
+        title: Text('Home'),
+        activeColor: Colors.red,
+      ),
+      BottomNavyBarItem(
+          icon: Icon(Icons.people),
+          title: Text('Users'),
+          activeColor: Colors.purpleAccent),
+      BottomNavyBarItem(
+          icon: Icon(Icons.message),
+          title: Text('Messages'),
+          activeColor: Colors.pink),
+      BottomNavyBarItem(
+          icon: Icon(Icons.settings),
+          title: Text('Settings'),
+          activeColor: Colors.blue),
+    ], onItemSelected: (index) {
+      return setState(() {
+          _index = index;
+        });
+    });
   }
 }
