9a91aa9a19230162a02d1c93cd51fcff330af706
hillary <hillary_ssemakula@student.uml.edu>
2019-03-23 00:21:50 -0400

    home nav 90% done

--- umlconfessions/lib/home.dart
+++ umlconfessions/lib/home.dart
@@ -7,103 +7,97 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  final profilePicture = 'assets/images/logo.png'; //profile picture
-  var karma = 100;
-  var numOfPosts = 16;
-  var postsArray = <Widget>[];
-  int navIndex = 0;
-  String appTitle = 'home';
-  Widget bodyWidget; // acts like fragment, whenever nav is pressed, this is changed to settings, profile, etc
+  final profilePicture = 'assets/images/logo.png'; //profile picture from firebae
+  int karma = 100; //karma value from firebase
+  int numOfPosts = 16; //number of posts user has from firebase
+  var postsArray = <Widget>[]; //might need, to store posts
+  int navIndex = 0; //keeps track of what menu is open
 
-  //To be used to generate and show posts.
-  Widget postsWidget() {
-    //return ListView.builder(itemBuilder: ,);
-    appTitle = "Home";
-    return Text('Posts Here');
-  }
-
-  //To show profile screen
-  Widget profileWidget(){
-    appTitle = 'Profile';
-    return Text("Profile Here");
-  }
-//bookmarks screen
-  Widget bookmarksWidget(){
-    appTitle = 'Bookmarks';
-    return Text("Bookmarks Here");
-  }
-
-  //settings screen
-  Widget settingsWidget(){
-    appTitle = 'Settings';
-    return Text("Settings Here");
-  }
-
+  //UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 33,
-                    width: 33,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage(profilePicture),
-                            fit: BoxFit.cover)),
-                  ),
-                  Container(
-                      margin: EdgeInsets.only(left: 20),
-                      child: Text(
-                        appTitle,
-                        style: TextStyle(color: Colors.black, fontSize: 18),
-                      ))
-                ]),
-            backgroundColor: Theme.of(context).canvasColor,
-            elevation: 2),
-        body: postsWidget(),
-        
-        
-        bottomNavigationBar:  BottomNavyBar(
-              items: navItems,
-              onItemSelected: (index) {
-                return setState(() {
-                  navIndex = index;
-                  switch(index){
-                    case 0: bodyWidget = postsWidget();
-                    break;
-                    case 1: bodyWidget = profileWidget();
-                    break;
-                    case 2: bodyWidget = bookmarksWidget();
-                    break;
-                    case 3: bodyWidget = settingsWidget();
-                    break;
-                  }
-                });
-              }),
-        );
+      appBar: AppBar(
+          title: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 33,
+                  width: 33,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage(profilePicture),
+                          fit: BoxFit.cover)),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 20),
+                  child: titleChildren[navIndex], //instead of title we have array, index 0(navIndex) is selected. This is the app bar title that corresponds to the first nav screen, etc
+                )
+              ]),
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 2),
+      body: bodyChildren[navIndex], //screen corresponding to nav element selected, is chosen from array
+      bottomNavigationBar: BottomNavyBar(
+          items: navItems,
+          onItemSelected: (index) {
+            return setState(() {
+              navIndex = index;
+            });
+          }),
+    );
   }
-}
 
-final navItems = <BottomNavyBarItem>[
-  BottomNavyBarItem(
-    icon: Icon(Icons.apps),
-    title: Text('Home'),
-    activeColor: Color(0xFF0072bc),
-  ),
-  BottomNavyBarItem(
-      icon: Icon(Icons.people),
-      title: Text('Profile'),
-      activeColor: Color(0xFF0072bc)),
-  BottomNavyBarItem(
-      icon: Icon(Icons.bookmark),
-      title: Text('Bookmarks'),
-      activeColor: Color(0xFF0072bc)),
-  BottomNavyBarItem(
-      icon: Icon(Icons.settings),
-      title: Text('Settings'),
-      activeColor: Color(0xFF0072bc)),
-];
+  //This array stores all nav icons. the Bottom navy function chooses one by index when you tap.
+  final navItems = <BottomNavyBarItem>[
+    BottomNavyBarItem(
+      icon: Icon(Icons.apps),
+      title: Text('Home'),
+      activeColor: Color(0xFF0072bc),
+    ),
+    BottomNavyBarItem(
+        icon: Icon(Icons.people),
+        title: Text('Profile'),
+        activeColor: Color(0xFF0072bc)),
+    BottomNavyBarItem(
+        icon: Icon(Icons.bookmark),
+        title: Text('Bookmarks'),
+        activeColor: Color(0xFF0072bc)),
+    BottomNavyBarItem(
+        icon: Icon(Icons.settings),
+        title: Text('Settings'),
+        activeColor: Color(0xFF0072bc)),
+  ];
+
+  //This array contains the different screens to be displayed, each index corresponds to a specific nav index when nav icon is pressed.
+  final bodyChildren = <Widget>[
+    Container(
+      constraints: BoxConstraints.expand(),
+      color: Colors.blue,
+      child: Text("Home"),
+    ),
+    Container(
+      constraints: BoxConstraints.expand(),
+      color: Colors.greenAccent,
+      child: Text("Profile"),
+    ),
+    Container(
+      constraints: BoxConstraints.expand(),
+      color: Colors.redAccent,
+      child: Text("Bookmarks"),
+    ),
+    Container(
+      constraints: BoxConstraints.expand(),
+      color: Colors.amberAccent,
+      child: Text("Settings"),
+    )
+  ];
+
+  //This array contains appbar titles and a specific index is chosen depending on nav icon slected.
+  final titleChildren = <Widget>[
+    Text("Home", style: TextStyle(color: Colors.black, fontSize: 18)),
+    Text("Profile", style: TextStyle(color: Colors.black, fontSize: 18)),
+    Text("Bookmarks", style: TextStyle(color: Colors.black, fontSize: 18)),
+    Text("Settings", style: TextStyle(color: Colors.black, fontSize: 18))
+  ];
+}
